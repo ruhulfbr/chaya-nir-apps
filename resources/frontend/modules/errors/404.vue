67ce19d1cbe77e88f90586550db3775d5bf917e7
ruhulfbr <ruhul11bd@gmail.com>
2024-12-25 01:59:10 +0600

    Make 404 page component

---
+++ resources/frontend/modules/errors/404.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <div class="d-flex align-items-center justify-content-center vh-100" style="background-color: #fff">
+        <div class="text-center" style="margin-top: -200px">
+            <h1 class="display-1 fw-bold">404</h1>
+            <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+            <p class="lead">
+                The page you’re looking for doesn’t exist.
+            </p>
+        </div>
+    </div>
+</template>
